f05c273be3b008b73c4035394bf0695ece7400e4
Verissimo Terceiro <terceiroverissimo@gmail.com>
2025-03-02 19:14:28 -0300

    configura formatacao padrao

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,11 @@
 function App() {
-    return (
-        <>
-            <div className='flex justify-center items-center h-screen'>
-                <h1 className='text-3xl font-bold underline'>Hello world!</h1>
-            </div>
-        </>
-    )
+  return (
+    <>
+      <div className="flex justify-center items-center h-screen">
+        <h1 className="text-3xl font-bold underline">Hello world!</h1>
+      </div>
+    </>
+  );
 }
 
-export default App
+export default App;
--- src/components/Dica.tsx
+++ src/components/Dica.tsx
@@ -1,34 +1,46 @@
-import { ReactNode } from "react";
+import { ReactNode } from 'react';
 
 interface DicaPropriedades {
-    visivel: boolean,
-    posicionamento?: string,
-    estilo?: string,
-    children: ReactNode
+  visivel: boolean;
+  posicionamento?: string;
+  estilo?: string;
+  children: ReactNode;
 }
 
-function Dica({visivel, posicionamento, estilo, children:conteudo}:DicaPropriedades) {
-    return (
-        <>
-            { visivel &&
-                <div className={"absolute -translate-y-full pb-2 " + posicionamento}>
-                    <div className={"bg-paleta-fundo border-2 border-paleta-auxiliar rounded-xl p-4 z-1 md:p-6 " + estilo}>
-                        {conteudo}
-                    </div>
-                </div>    
+function Dica({
+  visivel,
+  posicionamento,
+  estilo,
+  children: conteudo,
+}: DicaPropriedades) {
+  return (
+    <>
+      {visivel && (
+        <div className={'absolute -translate-y-full pb-2 ' + posicionamento}>
+          <div
+            className={
+              'bg-paleta-fundo border-2 border-paleta-auxiliar rounded-xl p-4 z-1 md:p-6 ' +
+              estilo
             }
-        </>
-    );
+          >
+            {conteudo}
+          </div>
+        </div>
+      )}
+    </>
+  );
 }
 
 interface TextoPropriedades {
-    children: string
+  children: string;
 }
 
-function Texto({children:texto}:TextoPropriedades) {
-    return (
-        <p className="text-t14 text-paleta-secundaria w-full md:text-t16">{texto}</p>
-    );
+function Texto({ children: texto }: TextoPropriedades) {
+  return (
+    <p className="text-t14 text-paleta-secundaria w-full md:text-t16">
+      {texto}
+    </p>
+  );
 }
 
 Dica.Texto = Texto;
