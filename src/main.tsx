b6863b500caa96694f80caa4a0f403ed0aeaed8e
Marianna <Marianna.lopes@academico.ifpb.edu.br>
2025-03-12 12:14:29 -0300

    Adiciona rota para informação de imóvel

--- src/main.tsx
+++ src/main.tsx
@@ -12,6 +12,7 @@ import { SessionProvider } from './context/authContext';
 import { Login } from './pages/autenticacao/Login';
 import { Pesquisa } from './pages/pesquisa/Pesquisa';
 import { PaginaMapa } from './pages/mapa/PaginaMapa';
+import { ImovelInfo } from './pages/imovel/ImovelInfo';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
@@ -29,6 +30,7 @@ createRoot(document.getElementById('root')!).render(
           <Route path="/pesquisa" element={<Pesquisa />} />
           <Route path="/mapa" element={<PaginaMapa />} />
           <Route path="/imovel/editar" element={<EditarImovel />} />
+          <Route path="/imovel/:id" element={<ImovelInfo />} />
         </Routes>
       </BrowserRouter>
     </SessionProvider>
