d23a2350bc1d68395cb282655333925fe107f938
Marianna <Marianna.lopes@academico.ifpb.edu.br>
2025-03-12 12:11:35 -0300

    Ajusta exposição de imagens

--- src/pages/imovel/ImovelInfo.tsx
+++ src/pages/imovel/ImovelInfo.tsx
@@ -29,20 +29,56 @@ function ImovelInfo() {
       <Cabecalho></Cabecalho>
       <div className="mt-32 w-[95%] grid justify-self-center">
         <span className="justify-self-end">
-          <Usuario id={''} nome={'Mari'} />
+          <Usuario id={''} nome={'Mari'} reverso />
         </span>
       </div>
 
       <div className="w-[80%] my-8 justify-self-center flex gap-14">
-        <img className="w-2/6 h-auto" src={imovel.imagem} />
-        <div className="flex overflow-x-auto shrink-0 gap-4">
-          <img className="w-20 h-auto" src={imovel.imagem} />
-          <img className="w-20 h-auto" src={imovel.imagem} />
-          <img className="w-20 h-auto" src={imovel.imagem} />
-          <img className="w-20 h-auto" src={imovel.imagem} />
-          <img className="w-20 h-auto" src={imovel.imagem} />
-          <img className="w-20 h-auto" src={imovel.imagem} />
-          <img className="w-20 h-auto" src={imovel.imagem} />
+        <img
+          className="w-[400px] aspect-square flex-shrink-0 border rounded-md border-paleta-secundaria"
+          src={imovel.imagem}
+        />
+        <div className="flex flex-row flex-wrap items-center content-center gap-6">
+          <img
+            className="w-28 aspect-square border border-paleta-secundaria rounded-md"
+            src={imovel.imagem}
+          />
+          <img
+            className="w-28 aspect-square border border-paleta-secundaria rounded-md"
+            src={imovel.imagem}
+          />
+          <img
+            className="w-28 aspect-square border border-paleta-secundaria rounded-md"
+            src={imovel.imagem}
+          />
+          <img
+            className="w-28 aspect-square border border-paleta-secundaria rounded-md"
+            src={imovel.imagem}
+          />
+          <img
+            className="w-28 aspect-square border border-paleta-secundaria rounded-md"
+            src={imovel.imagem}
+          />
+          <img
+            className="w-28 aspect-square border border-paleta-secundaria rounded-md"
+            src={imovel.imagem}
+          />
+          <img
+            className="w-28 aspect-square border border-paleta-secundaria rounded-md"
+            src={imovel.imagem}
+          />
+          <img
+            className="w-28 aspect-square border border-paleta-secundaria rounded-md"
+            src={imovel.imagem}
+          />
+          <img
+            className="w-28 aspect-square border border-paleta-secundaria rounded-md"
+            src={imovel.imagem}
+          />
+          <img
+            className="w-28 aspect-square border border-paleta-secundaria rounded-md"
+            src={imovel.imagem}
+          />
         </div>
       </div>
 
@@ -68,7 +104,6 @@ function ImovelInfo() {
         <div className="min-w-80 grow-6 flex flex-col gap-10">
           <div className="w-full flex-1 relative">
             <Mapa />
-            {/* colocar ver no mapa acima do mapa */}
             <a className="absolute bottom-4 left-4 text-paleta-secundaria underline decoration-paleta-secundaria hover:text-paleta-secundaria hover:no-underline hover:cursor-pointer">
               Ver no mapa
             </a>
