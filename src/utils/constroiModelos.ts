98d4799808123eb044fbed23e24a35483999cf6b
CaioPin <college13579@gmail.com>
2025-03-12 20:00:37 -0300

    adiciona comunicacao do componente mapa com api

--- src/utils/constroiModelos.ts
+++ src/utils/constroiModelos.ts
@@ -3,18 +3,23 @@ import { ImovelDTO, ImovelEnderecado } from '@/models/Imovel';
 import { UsuarioDTO, UsuarioPerfil } from '@/models/Usuario';
 import { enderecarImovel } from './enderecamento';
 
+function formataImagemImovel(imovel: ImovelDTO): ImovelDTO {
+  const imagens = imovel.imagens
+    ?.filter(imagem => imagem?.nomeImagem)
+    .map(imagem => ({
+      ...imagem,
+      nomeImagem: IMAGE_API_URL + imagem.nomeImagem,
+    }));
+
+  return { ...imovel, imagens };
+}
+
 async function organizaImoveis(imoveis: ImovelDTO[]) {
   const imoveisEnderecados: ImovelEnderecado[] = [];
 
   for (const imovel of imoveis) {
     const imovelEnderecado = await enderecarImovel(imovel);
-
-    const imagens = imovel.imagens
-      ?.filter(imagem => imagem?.nomeImagem)
-      .map(imagem => ({
-        ...imagem,
-        nomeImagem: IMAGE_API_URL + imagem.nomeImagem,
-      }));
+    const imagens = formataImagemImovel(imovel).imagens;
 
     imoveisEnderecados.push({ ...imovelEnderecado, imagens });
   }
@@ -45,4 +50,8 @@ async function construirModeloUsuarioPerfil(
   return { ...usuario, imoveis: imoveisEnderecados };
 }
 
-export { construirModeloUsuarioPerfil };
+function construirModeloImoveisMapa(imoveis: ImovelDTO[]): ImovelDTO[] {
+  return imoveis.map(formataImagemImovel);
+}
+
+export { construirModeloUsuarioPerfil, construirModeloImoveisMapa };
